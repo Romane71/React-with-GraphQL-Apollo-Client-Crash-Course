f432592ab4e52135592fabceed17554d379d61c5
Romane71 <romane.vanbroukhoven@aaoglobal.com>
2023-04-20 23:41:41 +0000

    Writing the first query ! (and it works)

---
+++ react-gql/src/pages/CharactersList.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { useQuery, gql } from "@apollo/client";
+import "./CharacterList.css";
+
+const GET_CHARACTERS = gql`
+  query {
+    characters {
+      results {
+        id
+        name
+        image
+      }
+    }
+  }
+`;
+
+export default function CharactersList() {
+  const { error, data, loading } = useQuery(GET_CHARACTERS);
+
+  console.log({ error, loading, data });
+
+  if (loading) return <div>spinner...</div>;
+  if (error) return <div>something went wrong</div>;
+
+  return (
+    <div className="CharacterList">
+      {data.characters.results.map((character) => {
+        return (
+          <div>
+            <img src={character.image} />
+            <h2>{character.name}</h2>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
